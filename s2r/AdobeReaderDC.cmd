817fa14b45f47a301104b31fb0c7bdddb9b7f1c6
LibDepo <88143043+LibDepo@users.noreply.github.com>
2021-10-19 13:13:34 +0300

    Add files via upload

--- s2r/AdobeReaderDC.cmd
+++ s2r/AdobeReaderDC.cmd
@@ -1,26 +1,24 @@
 REGEDIT4
 
-;@echo off& cls& cd /d "%~dp0"& echo/
+;@echo off& cls& cd /d "%~dp0"
 ;>nul 2>&1 dism|| (start cmd /c "color 0c& mode 45,4& @echo/& @echo    *** Требуются права Администратора! ***& @echo/& @pause"& exit)
 ;2>nul cd/d%windir%\sysnative&&(cmd/c"%~f0"&exit)
-;if not defined ProgramFiles(x86) (set dir=%ProgramFiles%) else set dir=%ProgramFiles(x86)%
-;set "dir=%dir%\Adobe\Acrobat Reader DC\Reader"
-;if exist "%dir%\AcroRd32.exe" goto tune
-;:load
-;if exist AdobeReaderDC\*.exe if exist AdobeReaderDC\*.msp goto install
-;2>nul md AdobeReaderDC& set url=https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC
-;>tst.vbs echo set x=CreateObject("Microsoft.XMLHTTP") : x.Open "GET", WSh.Arguments(0), False : x.Send : With CreateObject("Adodb.Stream") : .type = 1 : .open : .write x.responseBody : .savetofile WSh.Arguments(1), 2 : End With
-;if not exist AdobeReaderDC\*.exe echo Download Acrobat Reader...& cscript //nologo tst.vbs "%url%/1900820071/AcroRdrDC1900820071_ru_RU.exe" "AdobeReaderDC\AcroRdrDC1900820071_ru_RU.exe"
-;if not exist AdobeReaderDC\*.msp echo Download update...& cscript //nologo tst.vbs "%url%/2100720095/AcroRdrDCUpd2100720095.msp" "AdobeReaderDC\AcroRdrDCUpd2100720095.msp"
-;del /q tst.vbs
-;goto load
+;set "dir=%ProgramFiles(x86)%\Adobe\Acrobat Reader DC\Reader"
+;if exist AdobeReaderDC\*.exe goto install
+;2>nul md tmp& set url=https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC
+;>tmp\tst.vbs echo set x=CreateObject("Microsoft.XMLHTTP") : x.Open "GET", WSh.Arguments(0), False : x.Send : With CreateObject("Adodb.Stream") : .type = 1 : .open : .write x.responseBody : .savetofile WSh.Arguments(1), 2 : End With
+;cscript //nologo tmp\tst.vbs "https://libdepo.github.io/files/aria2c.cab" "tmp\aria.cab"
+;expand tmp\aria.cab -f:*.exe tmp
+;>tmp\list (
+;echo %url%/1900820071/AcroRdrDC1900820071_ru_RU.exe
+;echo %url%/2100720099/AcroRdrDCUpd2100720099.msp
+;)
+;tmp\aria2c -x8 -s8 -itmp\list -d AdobeReaderDC --file-allocation=trunc
+;rd /q/s tmp
 ;:install
-;AdobeReaderDC\AcroRdrDC1900820071_ru_RU.exe /sPB
-;AdobeReaderDC\AcroRdrDCUpd2100720095.msp /passive /norestart
-;:tune
-;sc stop AdobeARMservice
-;sc config AdobeARMservice start= disabled
-;schtasks /change /tn "Adobe Acrobat Update Task" /disable
+;AdobeReaderDC\AcroRdrDC1900820071_ru_RU.exe /sPB /msi DISABLE_ARM_SERVICE_INSTALL=1
+;AdobeReaderDC\AcroRdrDCUpd2100720099.msp /passive /norestart
+;schtasks /delete /tn "Adobe Acrobat Update Task" /f
 ;robocopy "%dir%\plug_ins" "%dir%\optional" Updater.api /mov
 ;>"%dir%\AcroApp\RUS\Viewer.aapp" echo ^<Application xmlns="http://ns.adobe.com/acrobat/app/2014" title="1" id="Viewer" majorVersion="1" requiresDoc="true" minorVersion="0"^>^</Application^>
 ;regedit /s "%~f0"
