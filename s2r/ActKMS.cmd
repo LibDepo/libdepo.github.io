a41f7ad98f41ccdb0f8ab621fc308162af81dd55
LibDepo <88143043+LibDepo@users.noreply.github.com>
2022-01-17 21:05:01 +0300

    Add files via upload

--- s2r/ActKMS.cmd
+++ s2r/ActKMS.cmd
@@ -1,12 +1,12 @@
 @echo off
 
 >nul 2>&1 dism|| (start cmd /c "color 0c& mode 45,4& @echo/& @echo    *** Требуются права Администратора! ***& @echo/& @pause"& exit)
-2>nul cd/d%windir%\sysnative&&(cmd/c"%~f0"&exit)
+if exist %windir%\sysnative\ %windir%\sysnative\cmd /c "%~f0"& exit
 
 cd /d "%~dp0"
 if exist KMS\*.cmd goto install
 2>nul md KMS
-set "url=https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS_1.4/All-In-One-Version/MAS_1.4_AIO_CRC32_9A7B5B05.cmd"
+set "url=https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_1.5_AIO_CRC32_21D20776.cmd"
 >tst.vbs echo set x=CreateObject("Microsoft.XMLHTTP") : x.Open "GET", WSh.Arguments(0), False : x.Send : With CreateObject("Adodb.Stream") : .type = 1 : .open : .write x.responseBody : .savetofile WSh.Arguments(1), 2 : End With
 cscript //nologo tst.vbs "%url%" "KMS\kms.cmd"
 del /q tst.vbs
