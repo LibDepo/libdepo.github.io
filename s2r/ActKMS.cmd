2995a2cc46e813bb38ec90b931eca7fa57088d7c
LibDepo <88143043+LibDepo@users.noreply.github.com>
2024-03-20 21:05:54 +0300

    Add files via upload

--- s2r/ActKMS.cmd
+++ s2r/ActKMS.cmd
@@ -4,11 +4,11 @@
 if exist %windir%\sysnative\ %windir%\sysnative\cmd /c "%~f0"& exit
 
 cd /d "%~dp0"
-if exist KMS\*.cmd goto install
-2>nul md KMS
-set "url=https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd"
+2>nul (rd /q/s KMS& md KMS)
+set "url=https://codeload.github.com/massgravel/Microsoft-Activation-Scripts/zip/refs/heads/master"
 >tst.vbs echo set x=CreateObject("Microsoft.XMLHTTP") : x.Open "GET", WSh.Arguments(0), False : x.Send : With CreateObject("Adodb.Stream") : .type = 1 : .open : .write x.responseBody : .savetofile WSh.Arguments(1), 2 : End With
-cscript //nologo tst.vbs "%url%" "KMS\kms.cmd"
-del /q tst.vbs
-:install
-call KMS\kms.cmd
+cscript //nologo tst.vbs "%url%" "tmp.zip"
+>tst.vbs echo With CreateObject("Shell.Application") .NameSpace(WSh.Arguments(0)).CopyHere(.NameSpace(WSh.Arguments(1)).Items) End With
+cscript //nologo tst.vbs "%CD%\KMS" "%CD%\tmp.zip"
+del /q *.vbs *.zip
+for /r %%i in (MAS_AIO*.cmd) do call "%%i"
